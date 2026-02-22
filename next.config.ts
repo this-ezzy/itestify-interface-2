5c8241e20e4825e31dd4e39ca50b33de9d2380c9
Ezekiel <geniuswealthb@gmail.com>
2026-02-22 13:06:56 +0100

    Chnges

--- next.config.ts
+++ next.config.ts
@@ -52,15 +52,17 @@ const nextConfig: NextConfig = {
     ]
   },
   images: {
-    domains: ["1214244a5427f71a53f3224f9a618cb3.r2.cloudflarestorage.com", "pub-31d5abaaeadc42e092b62816edbab470.r2.dev"],
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "/**",
+        protocol: 'https',
+        hostname: '1214244a5427f71a53f3224f9a618cb3.r2.cloudflarestorage.com',
+        pathname: '/**',
+
       },
       {
-        protocol: "https",
-        hostname: "/**",
+        protocol: 'https',
+        hostname: 'pub-31d5abaaeadc42e092b62816edbab470.r2.dev',
+        pathname: '/**',
       },
     ]
   },
